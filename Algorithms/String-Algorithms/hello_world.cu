a5b2cab3767e5b1c429780dfc967aff5da7d9e80
Georgy Trush <lkitjdp@gmail.com>
2021-09-14 13:33:49 +0300

    Update Hello World :wave:

--- Algorithms/String-Algorithms/hello_world.cu
+++ Algorithms/String-Algorithms/hello_world.cu
@@ -1,19 +1,56 @@
 #include <iostream>
 
+// Define execution configuration variables
+constexpr int numBlocksPerGrid = 1;
+constexpr int numThreadsPerBlock = 64;
+
 // Define hello world kernel
 __global__ void HelloWorldKernel() {
     printf("Hello World № %d!\n", threadIdx.x * gridDim.x);
 }
 
 int main() {
-    // Define execution configuration variables
-    int numBlocksPerGrid = 1, numThreadsPerBlock = 32;
+    // HOST EXECUTION
+
+    // Declare host clock variables
+	float elapsedTimeHost;
+    clock_t startTimeHost, stopTimeHost;
+
+    // Start host clock
+    startTimeHost = clock();
+
+    // Launch execution on host
+    for (int i = 0; i < numThreadsPerBlock; ++i) {
+        std::cout << "Hello World №" << i << "!\n";
+    }
+
+    // Stop host clock
+    stopTimeHost = clock();
+    elapsedTimeHost = (float) ((stopTimeHost) - (startTimeHost));
+	printf("Host Elapsed Time: %f ms\n", elapsedTimeHost);
+    
+    // DEVICE EXECUTION
+
+    // Declare device clock variables
+	float elapsedTimeDevice;
+    cudaEvent_t startTimeDevice, stopTimeDevice;
+
+    // Start device clock
+    cudaEventCreate(&startTimeDevice);
+	cudaEventRecord(startTimeDevice, 0);
 
     // Launch hello world kernel on device
-    HelloWorldKernel << <numBlocksPerGrid, numThreadsPerBlock >> > ();
+    HelloWorldNonSharedMemoryKernel <<<numBlocksPerGrid, numThreadsPerBlock>>> ();
 
-    // Wait for the device to finish
+    // Wait for the device to finish computing
     cudaDeviceSynchronize();
+
+    // Stop device clock
+    cudaEventCreate(&stopTimeDevice);
+	cudaEventRecord(stopTimeDevice, 0);
+	cudaEventSynchronize(stopTimeDevice);
+	cudaEventElapsedTime(&elapsedTimeDevice, startTimeDevice, stopTimeDevice);
+    printf("Device Elapsed Time: %f ms\n", elapsedTimeDevice);
 
     // Check for errors
     int exitStatus = EXIT_SUCCESS;
@@ -22,6 +59,6 @@ int main() {
         std::cout << "Error: " << cudaGetErrorString(err) << '\n';
         exitStatus = EXIT_FAILURE;
     }
-
+    
     return exitStatus;
 }
